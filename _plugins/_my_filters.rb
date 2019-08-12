ea40daf6e3ce1300b29a2cfe7860bb0536d4dd6e
Lex Robinson <lex@lex.me.uk>
2019-08-12 15:44:17 +0100

    Add vaguely functional jekyll version

---
+++ _plugins/_my_filters.rb
@@ -0,0 +1,15 @@
+require 'liquid'
+require 'uri'
+
+# Capitalize all words of the input
+module LexFilters
+  def entitle(words)
+    return words.gsub('-', ' ').split(' ').map(&:capitalize).join(' ')
+  end
+
+  def create_id(words)
+	return URI.decode(words).gsub(/\W+/, ' ').strip().gsub(" ", "-")
+  end
+end
+
+Liquid::Template.register_filter(LexFilters)
